07a35a429e3e96694ea055d60a2e023d08bfbb43
David Hund <david.hund@gmail.com>
2014-05-23 15:08:17 +0200

    Locally trying gh-pages Jekyll

--- gulp/tasks/jekyll-watch.js
+++ gulp/tasks/jekyll-watch.js
@@ -2,6 +2,11 @@ var gulp = require('gulp');
 
 gulp.task('jekyll-watch', function() {
 
-	require('child_process').spawn('jekyll', ['build', '--watch'], {stdio: 'inherit'});
+	// require('child_process').spawn('jekyll', ['build', '--watch'], {stdio: 'inherit'});
+
+	// Running with gh-pages specific Jekyll
+	// https://help.github.com/articles/using-jekyll-with-pages#installing-jekyll
+	// $> bundle exec jekyll build --watch
+	require('child_process').spawn('bundle', ['exec', 'jekyll', 'build', '--watch'], {stdio: 'inherit'});
 
 });
--- gulp/tasks/jekyll.js
+++ gulp/tasks/jekyll.js
@@ -4,6 +4,11 @@ var gulp = require('gulp');
 // From: http://stackoverflow.com/questions/21293999/use-jekyll-with-gulp
 gulp.task('jekyll', function() {
 
-	require('child_process').spawn('jekyll', ['build'], {stdio: 'inherit'});
+	// require('child_process').spawn('jekyll', ['build'], {stdio: 'inherit'});
+
+	// Running with gh-pages specific Jekyll
+	// https://help.github.com/articles/using-jekyll-with-pages#installing-jekyll
+	// $> bundle exec jekyll serve
+	require('child_process').spawn('bundle', ['exec jekyll build'], {stdio: 'inherit'});
 
 });
